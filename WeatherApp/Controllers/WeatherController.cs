8d0e46ef00f61059ba6b9d4ceaf59ba55be1fe98
Jacob <91929228+bjacobn@users.noreply.github.com>
2022-05-21 17:45:08 -0700

    Add search feature

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewWeather()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WeatherApp/Models/WeatherModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    public class WeatherModel
+    {
+        public string Country { get; set; }
+        public string city { get; set; }
+        public string Region { get; set; }
+        public string Timezone_id { get; set; }
+        public string Woeid { get; set; }
+
+
+        public string code { get; set; }
+        public string txt { get; set; }
+        public string temperature { get; set; }
+    }
+}
+
